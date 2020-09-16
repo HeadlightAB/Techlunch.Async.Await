aa099c2cc80afa74abf4785f427ddc640f4e9dc6
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2020-09-16 09:42:35 +0200

    Session 2 - Catch-when example

---
+++ Session 2/Example_3_Catch_When/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Example_3_Catch_When
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                try
+                {
+                    throw new IntegrationException(i);
+                }
+                catch (IntegrationException e) when (e.Status < 10)
+                {
+                    Console.WriteLine($"{e.Status} - Schade, zu viel!");
+                }
+                catch (IntegrationException e) when (e.Status > 9 && e.Status < 20)
+                {
+                    Console.WriteLine($"{e.Status} - Schade, immer zu viel!");
+                }
+                catch (IntegrationException e)
+                {
+                    Console.WriteLine($"{e.Status} - Ich gebe auf...");
+                }
+            }
+        }
+    }
+
+    public class IntegrationException : Exception
+    {
+        public int Status { get; set; }
+
+        public IntegrationException(int status)
+        {
+            Status = status;
+        }
+    }
+}
