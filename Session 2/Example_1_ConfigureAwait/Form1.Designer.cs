b64db79db0acb0c6cbb64b2763f34702bcb10f3d
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2020-09-16 08:43:20 +0200

    Session 2 - ConfigureAwait example

---
+++ Session 2/Example_1_ConfigureAwait/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Example_1_ConfigureAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConfigureAwaitTrueAkaDefault = new System.Windows.Forms.Button();
+            this.buttonConfigureAwaitFalse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConfigureAwaitTrueAkaDefault
+            // 
+            this.buttonConfigureAwaitTrueAkaDefault.Location = new System.Drawing.Point(190, 12);
+            this.buttonConfigureAwaitTrueAkaDefault.Name = "buttonConfigureAwaitTrueAkaDefault";
+            this.buttonConfigureAwaitTrueAkaDefault.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfigureAwaitTrueAkaDefault.TabIndex = 0;
+            this.buttonConfigureAwaitTrueAkaDefault.Text = "Go!";
+            this.buttonConfigureAwaitTrueAkaDefault.UseVisualStyleBackColor = true;
+            this.buttonConfigureAwaitTrueAkaDefault.Click += new System.EventHandler(this.buttonConfigureAwaitTrueAkaDefault_Click);
+            // 
+            // buttonConfigureAwaitFalse
+            // 
+            this.buttonConfigureAwaitFalse.Location = new System.Drawing.Point(190, 41);
+            this.buttonConfigureAwaitFalse.Name = "buttonConfigureAwaitFalse";
+            this.buttonConfigureAwaitFalse.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfigureAwaitFalse.TabIndex = 0;
+            this.buttonConfigureAwaitFalse.Text = "Go!";
+            this.buttonConfigureAwaitFalse.UseVisualStyleBackColor = true;
+            this.buttonConfigureAwaitFalse.Click += new System.EventHandler(this.buttonConfigureAwaitFalse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Run with default";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Run with ConfigureAwait(false)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 84);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConfigureAwaitFalse);
+            this.Controls.Add(this.buttonConfigureAwaitTrueAkaDefault);
+            this.Name = "Form1";
+            this.Text = "Demo: ConfigureAwait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConfigureAwaitTrueAkaDefault;
+        private System.Windows.Forms.Button buttonConfigureAwaitFalse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Session 2/Example_1_ConfigureAwait/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example_1_ConfigureAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonConfigureAwaitTrueAkaDefault_Click(object sender, EventArgs e)
+        {
+            await WriteRandomIntegers();
+
+            Console.WriteLine();
+            Console.WriteLine($"Done... @ ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine();
+        }
+
+        private async void buttonConfigureAwaitFalse_Click(object sender, EventArgs e)
+        {
+            await WriteRandomIntegersConfigureAwaitFalse();
+
+            Console.WriteLine();
+            Console.WriteLine($"Done... @ ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine();
+        }
+
+        private static async Task WriteRandomIntegers()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Creating integer {i}   @ ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(100);
+
+                var next = new Random().Next(99999999);
+
+                Console.WriteLine($"integer {i} - {string.Format("{0,8:D}", next)} @ ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine();
+            }
+        }
+
+        private static async Task WriteRandomIntegersConfigureAwaitFalse()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Creating integer {i}   @ ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(100).ConfigureAwait(false);
+
+                var next = new Random().Next(99999999);
+
+                Console.WriteLine($"integer {i} - {string.Format("{0,8:D}", next)} @ ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
